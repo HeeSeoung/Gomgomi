ac67ec3b56a16bef63c2d4488203dd2c7d974898
hiseoung <anhisg4104@gmail.com>
2022-04-15 16:28:51 +0900

    Add chat data db save

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,5 @@
-from attr import field
 from rest_framework import serializers
 from .models import chat_info
-from users.models import profile
 from django.contrib.auth.models import User
 
 
--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import chat_info
+from .serializers import ChatbotSerializer
 import requests, json
 
 # Create your views here.
@@ -12,12 +14,39 @@ class ChatbotView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication, )
 
+    def get(self, request):
+
+        user = request.user.id
+        queryset = chat_info.objects.filter(user = user)
+        serializers = ChatbotSerializer(queryset, many=True)
+
+        return Response(serializers.data)
+
     def post(self, request):
         context = {}
-        user = request.user.id
         sent = request.data['sent']
         data = {'sent': sent}
         response = requests.post('http://127.0.0.1:8000/predict', data=json.dumps(data))
         result = response.json()['response']
+        
+        create_chatinfo(
+            user = request.user.id,
+            context = sent,
+            chat_flag = 0
+        )
 
+        create_chatinfo(
+            user = request.user.id,
+            context = result,
+            chat_flag = 1,
+        )
         return Response(result)
+
+
+def create_chatinfo(**kwargs):
+    created = chat_info.objects.create(
+        user = kwargs.get('user', None),
+        context = kwargs.get('context', None),
+        chat_flag = kwargs.get('chat_flag', None)
+    )
+    return created

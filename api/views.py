c70c866059752e27e055dfa471f2aacec378b1a4
hiseoung <anhisg4104@gmail.com>
2022-06-06 19:00:41 +0900

    update voice_chatbot_api

--- api/views.py
+++ api/views.py
@@ -84,7 +84,7 @@ class VoiceChatbotView(APIView):
 
         # instantiates a client
         client = speech.SpeechClient()
-        audio = speech.RecognitionAudio(content=voice.read())
+        audio = speech.RecognitionAudio(content=voice.encode("ascii"))
 
         config = speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,

c8a4940dd4f202d6a84d2f4b27d53e9097f9c913
hiseoung <anhisg4104@gmail.com>
2022-06-10 14:01:59 +0900

    add user sentiment model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserSentiment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sentiment"
+    )
+    sentiment = models.IntegerField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    class Meta:
+        db_table = "user_sentiment"

3f39002b469da1cbcc03884310df8557ef5f9821
hiseoung <anhisg4104@gmail.com>
2022-06-17 01:44:20 +0900

    update

--- src/api/views.py
+++ src/api/views.py
@@ -141,7 +141,7 @@ class VoiceChatbotView(APIView):
         )
 
         response = HttpResponse(base64.b64encode(response.content))
-        response['result'] = {'text': result}
+        response['result'] = json.dump(result)
 
         return response
 
